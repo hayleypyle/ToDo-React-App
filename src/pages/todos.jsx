f4c34ffff49d7637081b27ba353c370b44cfbbc5
hayleypyle <hstowell@icloud.com>
2023-11-16 18:38:04 -0500

    basic html

---
+++ src/pages/todos.jsx
@@ -0,0 +1,26 @@
+
+function ToDos(){
+return(
+<>
+<h1>What ToDo?</h1>
+      <input type="text" />
+      <button>Add to List</button>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          Do Laundry
+          </li>
+        <li>
+          <input type="checkbox" />
+          Do Laundry
+        </li>
+        <li>
+          <input type="checkbox" />
+          Do Laundry
+        </li>
+
+      </ul>
+</>
+)
+}
+export default ToDos
