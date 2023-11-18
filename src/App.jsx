709c11ddd6e5319385c073438eae572c4476f88a
hayleypyle <hstowell@icloud.com>
2023-11-18 13:27:46 -0500

    troubleshooting gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import {HashRouter, Routes, Route, Link} from 'react-router-dom'
 import Contact from './pages/contact.jsx'
 import ToDos from './pages/todos.jsx'
 import Home from './pages/home.jsx'
@@ -13,15 +13,15 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <HashRouter>
     <header><Nav /></header>
     <Routes>
     <Route path='/' element={<Home />}>Home</Route>
     <Route path='/todos' element={<ToDos />}>To Do List</Route>
     <Route path='/contact' element={<Contact />}>Contact</Route>
-
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    
     
     </>
   )
